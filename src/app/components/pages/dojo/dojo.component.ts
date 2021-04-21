73d4b451dbb67ab053074c6025c624f3a12485b6
fanny <fanny.wastelin@hotmail.com>
2021-04-21 12:17:51 +0200

    debug event dnas page dojo, enlever bootstrap, rajout map dans page contact

--- src/app/components/pages/dojo/dojo.component.ts
+++ src/app/components/pages/dojo/dojo.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,16 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dojo.component.scss']
 })
 export class DojoComponent implements OnInit {
+  showProf: boolean =true
+  showDojo: boolean = false
+  showAssistant:boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  /*app.controller('myCtrl', function($scope) {
-    $scope.showMe = false;
-    $scope.functionTxt = function() {
-      $scope.showMe = !$scope.showMe;
-    }
-  }*/
+  eventShowProf(){
+    this.showProf = true
+    this.showDojo = false
+    this.showAssistant = false
+  }
+  eventShowDojo(){
+    this.showProf = false
+    this.showDojo = true
+    this.showAssistant = false
+  }
+  eventShowAssistant(){
+    this.showProf = false
+    this.showDojo = false
+    this.showAssistant = true
+  }
 
 }
